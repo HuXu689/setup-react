ebe607adb1f0e68fcc32612259b245535aa92db6
Nguyễn Xuân Huỳnh <xuanhuynh543@gmail.com>
2025-07-18 16:57:29 +0700

    fix: readme

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,16 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist', 'build', 'node_modules', 'react-router', 'coverage', 'public', 'out', 'lib']),
+  globalIgnores([
+    'dist',
+    'build',
+    'node_modules',
+    'react-router',
+    'coverage',
+    'public',
+    'out',
+    'lib',
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -21,16 +30,13 @@ export default tseslint.config([
     },
     rules: {
       // Cho phép dùng kiểu {}
-      // '@typescript-eslint/no-empty-object-type': 'off',
-
-      // // Cho phép dùng namespace (hoặc có thể migrate sang module sau)
-      // '@typescript-eslint/no-namespace': 'off',
-
-      // // Tắt bắt buộc chỉ export component trong file
-      // 'react-refresh/only-export-components': 'off',
-
-      // // Cho phép destructuring object rỗng ({} = ...)
-      // 'no-empty-pattern': 'off'
+      '@typescript-eslint/no-empty-object-type': 'off',
+      // Cho phép dùng namespace (hoặc có thể migrate sang module sau)
+      '@typescript-eslint/no-namespace': 'off',
+      // Tắt bắt buộc chỉ export component trong file
+      'react-refresh/only-export-components': 'off',
+      // Cho phép destructuring object rỗng ({} = ...)
+      'no-empty-pattern': 'off'
     },
   },
 ])
